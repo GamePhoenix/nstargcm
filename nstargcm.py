f3372ac6636b26aace7eb9d9c3bb8c113d46e198
GamePhoenix <stason.de@gmail.com>
2025-07-23 17:12:34 +0300

    Initial commit

---
+++ nstargcm.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.constants
+from matplotlib.animation import FuncAnimation
+from astropy import units as u
+
+class Star:
+    def __init__(self, teff : int, mass : int) -> None:
+        pass
+
+class Planet:
+    def __init__(self, albedo : int, mass : int, distance : int, radius : int):
+        pass
